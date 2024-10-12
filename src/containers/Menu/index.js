6ee90a5a77288416bec6fcf33b2683d9e5c3cd3d
ChakCodeMonkey <147180818+ChakCodeMonkey@users.noreply.github.com>
2024-10-12 15:42:27 +0200

    Beug Navbar corrigé ✅

--- src/containers/Menu/index.js
+++ src/containers/Menu/index.js
@@ -1,27 +1,38 @@
 /* eslint-disable no-return-assign */
 import Button from "../../components/Button";
 import Logo from "../../components/Logo";
-
 import "./style.scss";
 
-const Menu = () => (
-  <nav>
-    <Logo />
-    <ul>
-      <li>
-        <a href="#nos-services">Nos services</a>
-      </li>
-      <li>
-        <a href="#nos-realisations">Nos réalisations</a>
-      </li>
-      <li>
-        <a href="#notre-equipe">Notre équipe</a>
-      </li>
-    </ul>
-    <Button title="contact" onClick={() => (window.document.location.hash = "#contact")}>
-      Contact
-    </Button>
-  </nav>
-);
+const Menu = () => {
+  const scrollToSection = (id) => {
+    const section = document.getElementById(id);
+    if (section) {
+      section.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+  // ajout de cette variable pour gérer la transition qui nous amène vers le composant cliqué via le li dans la navbar//
+
+  return (
+    <nav>
+      <Logo />
+      <ul>
+        <li>
+          <a onClick={() => scrollToSection("nos-services")}>Nos services</a>
+        </li>
+        <li>
+          <a onClick={() => scrollToSection("nos-realisations")}>Nos réalisations</a>
+        </li>
+        <li>
+          <a onClick={() => scrollToSection("notre-equipe")}>Notre équipe</a>
+        </li>
+      </ul>
+      <Button title="Contact" onClick={() => scrollToSection("contact")}>
+        Contact
+      </Button>
+  
+    </nav>
+    // appel de la variable créée plus haut pour chaques li //
+  );
+};
 
 export default Menu;
